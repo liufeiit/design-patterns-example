60d0c5f37e0e9dde31c17ee01a257d39443446ce
Ethan Zhang <stillotherguy@qq.com>
2016-05-31 15:40:25 +0800

    initial commit

---
+++ src/main/java/com/rabbit/design/patterns/example/App.java
@@ -0,0 +1,13 @@
+package com.rabbit.design.patterns.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
